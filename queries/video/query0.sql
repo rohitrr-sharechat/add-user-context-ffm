97999b936dc656b2056506bf99713f259751e192
rohitrr <rohitrr@sharechat.co>
2021-06-15 05:02:53 +0000

    Changed base query

--- queries/video/query0.sql
+++ queries/video/query0.sql
@@ -14,10 +14,10 @@ vp_temp as (
   select
     * except (postId, userId, tagId, repeatCount, percentageFloat),
     ugc.postId, ugc.tagId, SAFE_CAST(userId as STRING) as userId,
-    IFNULL(repeatCount, 0) as repeatCount, IFNULL(percentageFloat, 0) as percentageFloat,
+    IFNULL(repeatCount, 0) as repeatCount, IFNULL(percentageFloat, 0) as percentageFloat
   from `maximal-furnace-783.sc_analytics.video_play` vp inner join ugc on CAST(vp.postId as STRING) = ugc.postId
   where 
-    time BETWEEN TIMESTAMP_SUB(TIMESTAMP('{end_time}'), interval {days} day) AND TIMESTAMP('{end_time}') AND
+    time BETWEEN TIMESTAMP_SUB(TIMESTAMP('{end_time}'), interval '{end_time}' day) AND TIMESTAMP('{end_time}') AND
     (percentageFloat IS NULL OR percentageFloat BETWEEN 0 AND 100) AND
     (duration IS NULL OR duration BETWEEN 0 AND 1e5) AND
     (repeatCount IS NULL OR repeatCount BETWEEN 0 AND 1e5)
@@ -31,7 +31,9 @@ vid_dur as (
 
 vp_succ as (
   select 
-    userId, postId, ANY_VALUE(vid_dur.tagId) as tagId,
+    userId, postId, ANY_VALUE(vid_dur.tagId) as tagId, 
+    extract(hour from MIN(time) at TIME ZONE "Asia/Kolkata") as hour_of_day,
+    extract(dayofweek from MIN(time) at TIME ZONE "Asia/Kolkata") as day_of_week,
     LOGICAL_OR(repeatCount > 1 OR (repeatCount > 0 AND vid_dur.duration*(percentageFloat/100) > 1)) as is_vp_succ,
     sum(if(vid_dur.duration between 5 and 19 and vid_dur.duration*(repeatCount+percentageFloat/100) > (((vid_dur.duration - 5) * 3.7189182474712084) / 15) + 10.95394866571609, 1,
           if(vid_dur.duration between 20 and 34 and vid_dur.duration*(repeatCount+percentageFloat/100) > (((vid_dur.duration - 20) * 7.005242627404698) / 15) + 20.989031673191437, 1,
@@ -44,7 +46,7 @@ vp_succ as (
           )
         )
     )=1 as is_vp_succ2,
-    LOGICAL_OR(vid_dur.duration*(repeatCount+percentageFloat/100) < 2) as is_vp_skip,
+    LOGICAL_OR(vid_dur.duration*(repeatCount+percentageFloat/100) < 5) as is_vp_skip,
   from vp_temp inner join vid_dur using (postId)
   where vid_dur.duration >= 5
   group by userId, postId
@@ -57,9 +59,25 @@ eng as (
     LOGICAL_OR(name = 'Post Shared-V2') AS is_share,
     LOGICAL_OR(name = 'Favourites') AS is_fav,
   from `maximal-furnace-783.sc_analytics.eng_view` inner join ugc using (postId)
-  where time BETWEEN TIMESTAMP_SUB(TIMESTAMP('{end_time}'), interval {days} day) AND TIMESTAMP('{end_time}')
+  where time BETWEEN TIMESTAMP_SUB(TIMESTAMP('{end_time}'), interval '{end_time}' day) AND TIMESTAMP('{end_time}')
   group by userId, postId
+),
+
+user_info as (
+    select id as userId, userCity, userState, CASE
+                                                when price_1 is not null and price_2 is not null then CAST((price_1+price_2)/2 as INT64) 
+                                                when price_1 is not null then price_1
+                                                when price_2 is not null then price_2
+                                                else null
+                                                end
+                                                as averagePhonePrice
+    from `maximal-furnace-783.sc_analytics.user` user
+    join `maximal-furnace-783.moj_analytics.phone_price_updated` phone
+    on user.phoneModel = phone.mobile_model_name
 )
 
-select *
+select * except (hour_of_day, day_of_week, userCity, userState, averagePhonePrice), concat(lower(IFNULL(userCity, "noCityLocation")), "_", lower(IFNULL(userState, "noStateLocation"))) as location_feature, 
+          concat(cast(RANGE_BUCKET(hour_of_day, [7, 9, 11, 14, 16, 19, 21]) as STRING), "_", cast(day_of_week as STRING)) as time_feature,
+          cast(IFNULL(RANGE_BUCKET(averagePhonePrice, [7499, 8999, 10999 ,14000]), -1) as STRING) as price_feature
 from vp_succ left join eng using (userId, postId, tagId)
+left join user_info using (userId);
--- queries/video/query2.sql
+++ queries/video/query2.sql
@@ -14,12 +14,17 @@ users as (
 --   from {q1table}
 -- ),
 
+referrers as (
+  select distinct CONCAT('3_referrer_', referrer) feature_name
+  from {q1table}
+),
+
 all_features as (
   (select * from users)
   union all
   (select * from posts)
-  -- union all
-  -- (select * from tags)
+  union all
+  (select * from referrers)
 )
 
 select feature_name, ROW_NUMBER() OVER (ORDER BY feature_name) as mapping
